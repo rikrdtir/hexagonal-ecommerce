482b8fbccae03dfb9108a4751aba4fecbb10ea4c
rikrdtir <ricardo6066@gmail.com>
2024-04-18 18:25:26 -0500

    First config spring app hexagonal architecute

---
+++ backend-spring-intentory-app/src/main/java/com/PerSEO/Hexagonalintentoryapp/HexagonalIntentoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.PerSEO.Hexagonalintentoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalIntentoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalIntentoryAppApplication.class, args);
+	}
+
+}
---
+++ backend-spring-intentory-app/src/test/java/com/PerSEO/Hexagonalintentoryapp/HexagonalIntentoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PerSEO.Hexagonalintentoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalIntentoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

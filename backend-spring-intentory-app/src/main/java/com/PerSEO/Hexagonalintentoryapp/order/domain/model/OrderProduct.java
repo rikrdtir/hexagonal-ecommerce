d0b1613b42800c77aed0cf745813267da2194ded
rikrdtir <ricardo6066@gmail.com>
2024-04-25 14:19:23 -0500

    create order model of module orders

---
+++ backend-spring-intentory-app/src/main/java/com/PerSEO/Hexagonalintentoryapp/order/domain/model/OrderProduct.java
@@ -0,0 +1,11 @@
+package com.PerSEO.Hexagonalintentoryapp.order.domain.model;
+
+import java.math.BigDecimal;
+
+public class OrderProduct {
+    private Integer id;
+    private BigDecimal quantity;
+    private BigDecimal price;
+    private Integer productId;
+    
+}

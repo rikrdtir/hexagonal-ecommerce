3b6a9e36e40353ebd83fd1772a59d3d922a71962
ricardt <ricardo6066@gmail.com>
2024-05-01 19:06:15 -0500

    add class order service and order repository

---
+++ backend-spring-intentory-app/src/main/java/com/PerSEO/Hexagonalintentoryapp/order/application/OrderService.java
@@ -0,0 +1,50 @@
+package com.PerSEO.Hexagonalintentoryapp.order.application;
+
+import com.PerSEO.Hexagonalintentoryapp.order.domain.model.Order;
+import com.PerSEO.Hexagonalintentoryapp.order.domain.port.IOrderRepository;
+
+public class OrderService {
+    private final IOrderRepository iOrderRepository;
+
+    public OrderService(IOrderRepository iOrderRepository) {
+        this.iOrderRepository = iOrderRepository;
+    }
+
+    /**
+     * Save order
+     * @param order
+     * @return
+     */
+    public Order save(Order order){
+        return this.iOrderRepository.save(order);
+    }
+
+    /**
+     * Find all orders
+     * @return
+     */
+
+    public Iterable<Order> findAll(){
+        return this.iOrderRepository.findAll();
+    }
+
+    /**
+     * Find order by id
+     * @param userId
+     * @return
+     */
+    public Iterable<Order> findByUserId(Integer userId){
+        return this.iOrderRepository.findByUserId(userId);
+    }
+
+    /**
+     * Update state order
+     * @param id
+     * @param state
+     */
+    public void updateStateById(Integer id, String state){
+        this.iOrderRepository.updateStateById(id, state);
+    }
+
+
+}
--- backend-spring-intentory-app/src/main/java/com/PerSEO/Hexagonalintentoryapp/order/domain/port/IOrderRepository.java
+++ backend-spring-intentory-app/src/main/java/com/PerSEO/Hexagonalintentoryapp/order/domain/port/IOrderRepository.java
@@ -1,5 +1,12 @@
 package com.PerSEO.Hexagonalintentoryapp.order.domain.port;
 
+import com.PerSEO.Hexagonalintentoryapp.order.domain.model.Order;
+
 public interface IOrderRepository {
+    Order save(Order order);
+    Order findById(Integer id);
+    Iterable<Order> findAll();
+    Iterable<Order> findByUserId(Integer userId);
+    void updateStateById(Integer id, String state);
     
 }

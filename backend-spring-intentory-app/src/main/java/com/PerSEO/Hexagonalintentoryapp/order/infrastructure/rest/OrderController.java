e47d47011d9f29b8b8388b3fe377272e9a144cce
rikrdtir <ricardo6066@gmail.com>
2024-05-12 21:53:56 -0500

    add  class rest orderController

---
+++ backend-spring-intentory-app/src/main/java/com/PerSEO/Hexagonalintentoryapp/order/infrastructure/rest/OrderController.java
@@ -0,0 +1,41 @@
+package com.PerSEO.Hexagonalintentoryapp.order.infrastructure.rest;
+
+import com.PerSEO.Hexagonalintentoryapp.order.application.OrderService;
+import com.PerSEO.Hexagonalintentoryapp.order.domain.model.Order;
+import com.PerSEO.Hexagonalintentoryapp.order.domain.model.OrderState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/orders")
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    @PostMapping
+    public ResponseEntity<Order> save(@RequestBody Order order){
+        if (order.getOrderState().toString().equals(OrderState.CANCELLED.toString())){
+            order.setOrderState(OrderState.CANCELLED);
+
+        }else {
+
+            order.setOrderState(OrderState.CONFIRMED);
+
+        }
+        return ResponseEntity.ok(orderService.save(order));
+    }
+    @PostMapping("/update/state/order")
+    public ResponseEntity updateStateById(@RequestParam Integer id, @RequestParam String state){
+        orderService.updateStateById(id,state);
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping
+    public ResponseEntity<Iterable<Order>> finAll(){
+        return ResponseEntity.ok(orderService.findAll());
+    }
+}

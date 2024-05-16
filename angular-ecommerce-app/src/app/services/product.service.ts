4f9858e5bc8e017fcbf6718395cfac1aa4bee413
rikrdtir <ricardo6066@gmail.com>
2024-05-16 12:37:52 -0500

    Create product service and all methods

--- angular-ecommerce-app/src/app/services/product.service.ts
+++ angular-ecommerce-app/src/app/services/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl: string = "http://localhost:8085/api/v1_1/admin/products"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.apiUrl);
+
+  }
+
+
 }
